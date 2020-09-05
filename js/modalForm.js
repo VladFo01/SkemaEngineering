14ba18a3f234049f30fcee8545af16fc3c82492f
Batman1.0 <62206631+VladFo01@users.noreply.github.com>
2020-09-05 23:20:54 +0300

    Add files via upload

---
+++ js/modalForm.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('.header_modal-form_wrapper');
+const formBody = document.querySelector('.header_modal-form');
+const formClose = document.querySelector('.form-close');
+const contactBtn = document.querySelector('.contacts_button');
+
+const addClass = (block, className) => {
+  block.classList.add(className);
+}
+
+const removeClass = (block, className) => {
+  block.classList.remove(className);
+}
+
+contactBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  document.body.classList.add('lock');
+  form.classList.add('form-open');
+  form.classList.add('header_modal-form_wrapper-active');
+  setInterval(addClass(form, 'header_modal-form_wrapper-active_1'), 1000);
+  setInterval(addClass(formBody, 'header_modal-form-active'), 1000);
+  setInterval(addClass(formBody, 'header_modal-form-active_1'), 1000);
+});
+
+formClose.addEventListener('click', () => {
+  removeClass(form, 'form-open');
+  document.body.classList.remove('lock');
+});
+form.addEventListener('click', (event) => {
+  let box = formBody.getBoundingClientRect();
+
+  if(event.clientX > box.right || event.clientX < box.left || event.clientY > box.bottom || event.clientY < box.top){
+    removeClass(form, 'form-open');
+    document.body.classList.remove('lock');
+  }
+});
