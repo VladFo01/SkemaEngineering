c0b6d2e54f1d23f29dd53af9dd50c7ba1b5c42a5
Batman1.0 <62206631+VladFo01@users.noreply.github.com>
2020-09-11 22:17:27 +0300

    Add files via upload

---
+++ js/typeWriter.js
@@ -0,0 +1,46 @@
+var mess=new Array(
+  "Проектируем ",
+  "Строим ",
+  "Согласовуем ");
+ 
+ var size = 32;
+ var txtcol = "#fff";
+ var lastcol = "#C0C0C0";
+ var pause = 1500;
+ var speed = 100;
+ var i = i_str = 0;
+ var msg = msgpre = msgafter = "";
+ 
+ function get_text() {
+  msgpre = mess[i].substring(0, i_str-1);
+  msgafter = mess[i].substring(i_str-1, i_str);
+  msg = "<span style='position: relative; color:" + txtcol + ";'>" + msgpre + "<span style='color:" + lastcol + ";'>" + msgafter +
+  "</span></span>";
+ }
+ 
+ function go() {
+ if (i_str<=mess[i].length-1) {
+  i_str++;
+  get_text();
+  if (document.all) typewriter.innerHTML = msg;
+  else if (document.layers) {
+   document.typewriter.document.write(msg);
+   document.typewriter.document.close();
+  }
+  else document.getElementById("typewriter").innerHTML = msg;
+  var timer = setTimeout("go()", speed);
+ }
+ else {
+  clearTimeout(timer);
+  var timer = setTimeout("changemess()", pause);
+ }
+ }
+ 
+ function changemess() {
+  i++;
+  i_str = 0;
+  if (i>mess.length-1) i = 0;
+  go();
+ }
+
+ go();
